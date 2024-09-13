22fd1309c092ffe4fee18e31e4039345f99699d5
anulalat <ndra.bodoh@gmail.com>
2024-09-13 10:38:20 +0700

    Revert "Camera: Flush Improvements - Result drain optimization"
    This reverts commit 0987bef9f316d6ae0ce8da64d8c0b0a0bc5828a8.

--- services/camera/libcameraservice/device3/Camera3Device.cpp
+++ services/camera/libcameraservice/device3/Camera3Device.cpp
@@ -3313,15 +3313,9 @@ status_t Camera3Device::RequestThread::clear(
 
 status_t Camera3Device::RequestThread::flush() {
     ATRACE_CALL();
-    status_t flush_status;
     Mutex::Autolock l(mFlushLock);
 
-    flush_status = mInterface->flush();
-    // We have completed flush, signal RequestThread::waitForNextRequestLocked() to no longer wait for
-    // new requests
-    mRequestSignal.signal();
-
-    return flush_status;
+    return mInterface->flush();
 }
 
 void Camera3Device::RequestThread::setPaused(bool paused) {
